91621db287233453c5f0d56fe2085a48eb35f832
DTadrzak <daniel.tadrzak@intel.com>
2016-01-04 14:33:40 +0100

    Update upgrade-centos-to-newest-kernel.sh
    Added necessary -y to yum install.

--- serenity/files/mesos/upgrade-centos-to-newest-kernel.sh
+++ serenity/files/mesos/upgrade-centos-to-newest-kernel.sh
@@ -4,7 +4,7 @@
 
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
-yum --enablerepo=elrepo-kernel install kernel-ml
+yum --enablerepo=elrepo-kernel install -y kernel-ml
 #Once the installation is finished, make sure you have got new kernel.
 
 awk -F\' '$1=="menuentry " {print $2}' /etc/grub2.cfg
